97b88641451e8d2d42ea33e244218794d9c731af
Ayub Hareed <69177111+A-Hareed@users.noreply.github.com>
2024-07-03 13:13:14 +0800

    Update dnn_model2.py

--- dnn_model2.py
+++ dnn_model2.py
@@ -41,6 +41,51 @@ test_feat_12 = create_feature_set(test_feat,12,3)
 
 del data_bb, data_target
 
+def create_model_LeakyRELU(input_shape, num_outputs):
+    inputs = tf.keras.Input(shape=input_shape)
+    x = tf.keras.layers.Dense(2048)(inputs)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+
+    x = tf.keras.layers.Dense(1024)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    
+    x = tf.keras.layers.Dense(512)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+    
+    x = tf.keras.layers.Dense(256)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Dense(256)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Dense(128)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Dense(128)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    x = tf.keras.layers.Dense(64)(x)
+    x = tf.keras.layers.LeakyReLU(alpha=0.2)(x)
+    x = tf.keras.layers.BatchNormalization()(x)
+    x = tf.keras.layers.Dropout(0.2)(x)
+
+    output = tf.keras.layers.Dense(num_outputs, activation='linear')(x)
+    
+    model = tf.keras.Model(inputs=inputs, outputs=output)
+    return model
 
 def create_model_2(input_shape, num_outputs):
     inputs = tf.keras.Input(shape=input_shape)
