b1a96f53e90dfdeb277809ab2639251e1157e13c
A-Hareed <Ayubm56@gmail.com>
2024-08-22 15:46:26 +0800

    fixed the no_Atoms dictionary

--- SideChain_datapreparation.py
+++ SideChain_datapreparation.py
@@ -13,20 +13,20 @@ no_Atoms= {
  'MET':[4,[0]],
  'ASP':[4,[0]],
  'ASN':[4,[0]],
- 'ARG':[7,['0_3','3_8']],
+ 'ARG':[7,['0,1,2,3,4,5,6,7,8','9,10,11,12,13,14,15,16,17,18,19,20']],
  'GLN':[5,[0]],
  'GLU':[5,[0]],
  'GLY':[0,[0]],
- 'HIS':[6,['0_2','2_4','4_6']],
+ 'HIS':[6,['0,1,2,3,4,5','6,7,8,9,10,11','12,13,14,15,16,17']],
  'ILE':[4,[0]],
  'LEU':[4,[0]],
- 'LYS':[5,['0_3','3_5']],
- 'PHE':[7,['0_3','3_5','5_7']],
+ 'LYS':[5,['0,1,2,3,4,5,6,7,8','9,10,11,12,13,14']],
+ 'PHE':[7,['0,1,2,3,4,5,6,7,8','9,10,11,12,13,14','5,16,17,18,19,20']],
  'PRO':[3,[0]],
  'SER':[2,[0]],
  'THR':[3,[0]],
- 'TRP':[10,['0,1,9','2_5','7,8','5,6']],
- 'TYR':[8,['0_3','3_6','6_8']],
+ 'TRP':[10,['0,1,2,3,4,5,27,28,29','6,7,8,9,10,11,12,13,14','21,22,23,24,25,26','15,16,17,18,19,20']],
+ 'TYR':[8,['0[0,1,2],1[3,4,5],2[6,7,8]','3[9,10,11],4[12,13,14],5[15,16,17]','6[18,19,20],7[21,22,23']],
  'VAL':[3,[0]]
  }
 
@@ -39,18 +39,45 @@ start_indx = 0
 
 for i in range(0,len(sequence)):
     number_at = no_Atoms[sequence[i]]
+    end_indx = start_indx + ( number_at[0]*3)
     if isinstance(number_at[1][0], int):
-        print(number_at[1][0],number_at[0]*3)
+
         padding_amount = 15 - (number_at[0]*3)
-        end_indx = start_indx + ( number_at[0]*3)
         temp_arr = np.pad(arr_data[:,start_indx:end_indx], pad_width=((0, 0), (0, padding_amount)), mode='constant', constant_values=0)
-        print(temp_arr.shape, arr_data[:,start_indx:end_indx].shape)
+        if 'final_arr' in globals():
+            final_arr = np.concatenate((final_arr,temp_arr),axis=1)
+        else:
+            final_arr = temp_arr
+    else:
+        residue_arr = arr_data[:,start_indx:end_indx]
+        for num, bead in enumerate(number_at[1]):
+            if ',' in bead:
+                bead = [int(j) for j in bead.split(',')]
+                padding_amount = 15 - (len(bead)*3)
+                singel_bead = np.pad(residue_arr[:,bead], pad_width=((0, 0), (0, padding_amount)), mode='constant', constant_values=0)
+                    
+            elif '_' in bead:
+                
+                slicing = [int(j) for j in bead.split('_')]
+                padding_amount = 15 - ((slicing[1]-slicing[0])*3)
+                singel_bead = np.pad(residue_arr[:,slicing[0]:slicing[1]], pad_width=((0, 0), (0, padding_amount)), mode='constant', constant_values=0)
+                print(padding_amount, singel_bead.shape, slicing,residue_arr[:,slicing[0]:slicing[1]].shape)
+
+            if num == 0:
+                temp_arr = np.copy(singel_bead)
+            elif num >0:
+                temp_arr = np.concatenate((temp_arr,singel_bead),axis=1)
+
         if 'final_arr' in globals():
             final_arr = np.concatenate((final_arr,temp_arr),axis=1)
         else:
             final_arr = temp_arr
 
-print(final_arr.shape, final_arr.reshape(-1,15).shape)
+
+    start_indx=end_indx
+['0_3','3_6','6_8']
+print(final_arr.shape,final_arr.reshape(-1,15).shape)
+print(final_arr[0,-15:],temp_arr[0,:])
 padded_arr = np.pad(arr, pad_width=((0, 0), (0, 3)), mode='constant', constant_values=0)
-print(temp_arr[0])
+print(temp_arr[0],start_indx)
 # print(padded_arr,padded_arr.shape)
