1784732e20270cadf3ef767a8005e06824e8840f
Ayub Hareed <69177111+A-Hareed@users.noreply.github.com>
2025-02-28 13:14:57 +0800

    Update gromacs_singularity.sh

--- gromacs_singularity.sh
+++ gromacs_singularity.sh
@@ -11,3 +11,29 @@ sudo apt update
 
 singularity --version
 apptainer version 1.2.5
+
+
+
+
+Bootstrap: docker
+From: ubuntu:20.04
+
+%post
+    apt-get update
+    apt-get install -y wget build-essential cmake gfortran
+    wget https://ftp.gromacs.org/pub/gromacs/gromacs-2021.8.tar.gz
+    tar xzf gromacs-2021.8.tar.gz
+    cd gromacs-2021.8
+    mkdir build
+    cd build
+    cmake .. -DGMX_BUILD_SHARED_LIBS=ON -DGMX_MPI=OFF
+    make -j $(nproc)
+    make install
+    export PATH="/usr/local/gromacs/bin:$PATH"
+    gmx --version
+
+%environment
+    export PATH="/usr/local/gromacs/bin:$PATH"
+
+%runscript
+    gmx "$@"
